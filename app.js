7109891c4ef27ff1474deb7d58dacccef39260b3
Jue Ning <juening@gmail.com>
2016-07-14 22:11:00 -0400

    done

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ var monthsArr = ["January", "February", "March", "April", "May", "June", "July",
 
 var colors = ["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"];
 
-var marginLeft = 80, marginRight = 20, marginTop = 20, marginBottom = 40;
+var marginLeft = 80, marginRight = 20, marginTop = 20, marginBottom = 70;
 
 var height = h - marginBottom - marginTop;
 
 var width = w - marginLeft - marginRight;
 
-var toolTip = function(obj){
-    return " " + obj.year + " " + monthsArr[obj.month-1] + " " + " " + obj.variance;
-}
+
 
 d3.json(dataUrl, function(error, data){
     if(error){
@@ -29,7 +27,11 @@ d3.json(dataUrl, function(error, data){
         var highTemp = d3.max(temps);
         var colorScale = d3.scale.quantile().domain([lowTemp, highTemp]).range(colors);
         
-        console.log(temps);
+        var toolTip = function(obj){
+            return " " + obj.year + " " + monthsArr[obj.month-1] + " " + "Variance: " + obj.variance + "  Temperature: " + (obj.variance + baseTemp);
+            };
+        
+        //console.log(temps);
         
         var svg = d3.select("body").append("svg").attr("width", w).attr("height", h).append("g")
                     .attr("transform", "translate(" + marginLeft + "," + marginTop + ")");;
@@ -37,6 +39,8 @@ d3.json(dataUrl, function(error, data){
         var yScale = d3.scale.ordinal().domain(monthsArr).rangeRoundBands([0, height]);
         
         var years = dataset.map(function(item){return item.year;});
+        
+        //eliminate dupplicated years
         years = years.filter(function(item, index){ return years.indexOf(item) === index; });
         
         var minYear = d3.min(years);
@@ -53,8 +57,8 @@ d3.json(dataUrl, function(error, data){
         
         var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(10);
         
-        svg.append("g").attr("class", "axis").call(yAxis);
-        svg.append("g").attr("class", "axis").attr("transform", "translate(0," + height+ ")").call(xAxis);
+        svg.append("g").attr("class", "axis").call(yAxis);//yaxis
+        svg.append("g").attr("class", "axis").attr("transform", "translate(0," + height+ ")").call(xAxis);//xaxis
         
         var gridWidth = width / years.length;
         var gridHeight = height / monthsArr.length;
@@ -69,5 +73,36 @@ d3.json(dataUrl, function(error, data){
                       }).append("title").text(function(d){return toolTip(d);})
                       .on("mouseover", function(){});
         
+        //x-label        
+        svg.append("g")
+            .attr("transform", "translate(-50," + height/2 + ")")
+            .append("text").attr("text-anchor", "middle")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .text("Months");
+        
+        //y-label
+        svg.append("g")
+            .attr("transform", "translate(" + width/2 + ", " + (height + marginBottom/2) + ")")
+            .append("text")
+            .attr("text-anchor", "middle")
+            .attr("class", "label")
+            .text("Years");
+        
+        //legend
+        var legend = svg.selectAll("legend")
+            .data([0].concat(colorScale.quantiles()), function(d){return d;})
+            .enter()
+            .append("g");
+        
+        legend.append("rect")
+            .attr("x", function(d, i){ return i * 30 + width / 2 + 30;})//20px wide per rect
+            .attr("y", function(d){ return height + marginBottom/2;}).attr("width", 30).attr("height", 20).style("fill", function(d, i){return colors[i];});
+        
+        legend.append("text").attr("class", "axis")
+            .text(function(d){return d.toPrecision(3);})
+            .attr("x", function(d, i){ return i * 30 + width / 2 + 30;})//20px wide per rect
+            .attr("y", function(d){ return (height + marginBottom/2 + 35);})
+            ;
     }
 })
